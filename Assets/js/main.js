4e40e07a169fa758f6976ac31aa34b3299848df7
Simon Woodward <simonjwoodward5@gmail.com>
2024-06-29 16:44:56 +0100

    Continued refactoring file load

---
+++ Assets/js/main.js
@@ -0,0 +1,27 @@
+function loadExtraHTML(file, element) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(element).innerHTML = data;
+            const scripts = document.querySelectorAll('#'.concat(element).concat(' script'));
+            scripts.forEach(script => {
+                const newScript = document.createElement('script');
+                newScript.src = script.src;
+                document.head.appendChild(newScript);
+                });
+        });
+    }
+
+document.addEventListener("DOMContentLoaded", function() {
+    let files = [{
+        file: 'header.html',
+        element: 'header-placeholder'
+    }, 
+    {
+        file: 'footer.html',
+        element: 'footer-placeholder'
+    }];
+    files.forEach(file => {
+        loadExtraHTML(file.file, file.element);
+    });
+});
