9f5608cfcac3549d6c5e10ceb339de169917b4e9
Simon Woodward <Simonjwoodward5@gmail.com>
2024-10-06 19:37:22 +0100

    Fixed preferences file

--- scripts/server.js
+++ scripts/server.js
@@ -256,7 +256,7 @@ app.get('/api/getStories', (req, res) => {
         const lines = data.split('\n');
         const stories = [];
         let storyMaxNumber = 5; //gives default value of 5
-        fs.readFile('./Assets/preferences.json', 'utf8', (err, pref) => {
+        fs.readFile('./Assets/Data/preferences.json', 'utf8', (err, pref) => {
             if(!err)
                 {
                     storyMaxNumber = pref.numberOfPostsOnMainPage;
