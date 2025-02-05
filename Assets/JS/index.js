64491857b72ea1622c33c46f6b41f2024b4850cb
Simon Woodward <simonjwoodward5@gmail.com>
2025-02-05 18:07:34 +0000

    Changed story colour to lilac

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -13,7 +13,7 @@ function loadEvents() {
     function createEventDiv(title, description, imgSrc, imgAlt, id) {
         // Create the main container div
         const containerDiv = document.createElement('div');
-        containerDiv.className = 'p-4 p-md-4 mb-4 text-white bg-dark rounded';
+        containerDiv.className = 'p-4 p-md-4 mb-4 text-dark bg-dark rounded';
     
         // Create the row div
         const rowDiv = document.createElement('div');
@@ -51,7 +51,7 @@ function loadEvents() {
         // Create the anchor element
         const anchor = document.createElement('a');
         anchor.href = '/stories/' + id + '_' + title.replace(/\s+/g, '_');
-        anchor.className = 'text-white fw-bold';
+        anchor.className = 'text-black fw-bold';
         anchor.textContent = 'Continue reading...';
     
         // Append the anchor to the second paragraph
